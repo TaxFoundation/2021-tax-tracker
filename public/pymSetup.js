67a0374e51fc81354edc6fb2997085d781337393
Tom VanAntwerp <tvanantwerp@users.noreply.github.com>
2021-06-15 10:08:44 -0400

    Fix id in pym setup

--- public/pymSetup.js
+++ public/pymSetup.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", function () {
   const pymParent = new pym.Parent(
-    "2021-tracker",
+    "2021-tax-tracker",
     "https://2021-tax-tracker.netlify.com/index.html",
     {}
   );
